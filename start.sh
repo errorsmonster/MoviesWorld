989894ce824641b359fb170306e5855e1e840417
Arunachalam <l20cs001@vvcoe.org>
2023-08-11 08:45:53 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ else
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
 fi
 cd /DQTheFileDonorBot
+pip install --upgrade pip
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
