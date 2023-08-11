01b974f8a5d9c9271fa422fe3dca23b293da6d32
Arunachalam <l20cs001@vvcoe.org>
2023-08-11 20:12:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ else
 fi
 cd /DQTheFileDonorBot
 pip install --upgrade pip
-pip3 install -r requirements.txt
+pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
