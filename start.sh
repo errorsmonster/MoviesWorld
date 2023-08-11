c70190aa1dfbb02ebebc1cdedc3e03ea77fff9b6
Arunachalam <l20cs001@vvcoe.org>
2023-08-11 08:34:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQTheFileDonorBot
 fi
-cd /DQTheFileDonorBot
+cd /DQ-the-file-donor-bot
 pip3 install -U -r requirements.txt
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
