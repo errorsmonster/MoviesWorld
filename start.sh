b40f76e7ab03e8592334a929b154ac99452bd715
Arunachalam <l20cs001@vvcoe.org>
2023-09-06 10:21:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,23 @@
-if [ -z $UPSTREAM_REPO ]
+if [ -z "$UPSTREAM_REPO" ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/akarunachalamprogrammer/DQ-the-file-donor-bot
+  git clone https://github.com/akarunachalamprogrammer/DQ-the-file-donor-bot DQ-the-file-donor-bot
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQTheFileDonorBot
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone "$UPSTREAM_REPO" DQ-the-file-donor-bot
 fi
-cd /DQ-the-file-donor-bot
-pip install --upgrade pip
-pip3 install -U -r requirements.txt
+
+cd DQ-the-file-donor-bot  # Change to the correct directory
+
+# Check if 'requirements.txt' exists before attempting to install it
+if [ -e requirements.txt ]
+then
+  pip install --upgrade pip
+  pip install -U -r requirements.txt
+else
+  echo "ERROR: 'requirements.txt' not found in the current directory."
+  exit 1  # Exit the script with an error code
+fi
+
 echo "Starting DQ-The-File-Donor...."
 python3 bot.py
