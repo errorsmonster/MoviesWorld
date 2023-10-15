b790f372bb9e9471aaf862a47f5da1412b4a11d8
Arunachalam <l20cs001@vvcoe.org>
2023-10-15 21:19:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -70,7 +70,7 @@ async def pm_text(bot, message):
     user_id = message.from_user.id
     if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
     if user_id in ADMINS: return # ignore admins
-    await message.reply_text("<b>Please Use This Bot 👉 https://t.me/The_Publishers_bot or  Search This Group 👉 https://t.me/+Z1yPQ4qCxoZjNGY1</b>")
+    await message.reply_text("<b>Please Use This Bot 👉 https://t.me/The_Publishers_bot \n or \n Search This Group 👉 https://t.me/+Z1yPQ4qCxoZjNGY1</b>")
     await bot.send_message(
         chat_id=LOG_CHANNEL,
         text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
