d077fbd47b7ce624c42ec8e6e44babadae3eedfb
errorsmonster <151611578+errorsmonster@users.noreply.github.com>
2023-11-24 15:59:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -42,8 +42,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = is_enabled((environ.get("NO_RESULTS_MSG", 'False')), False)
 
 # MongoDB information
-DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://monsterprogrammer:S.Aruna1155182089@thepublishers.kycudae.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "ThePublishers")
+DATABASE_URL = environ.get('DATABASE_URL', "mongodb+srv://akmonsterprogrammer:S.Aruna1155182089@movies.9fplh9i.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "Movies")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
